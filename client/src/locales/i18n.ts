becbdc07a320df0ef1a1af02db3eafc6a25d350a
kyreal <kyreal@MacBook-Air-kyreal.local>
2026-03-13 12:56:34 +0300

    fix rendering

--- client/src/locales/i18n.ts
+++ client/src/locales/i18n.ts
@@ -1,11 +1,13 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import ru from './ru.json'
 import en from './en.json'
 import uk from './uk.json'
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
@@ -13,11 +15,15 @@ i18n
       en: { translation: en },
       uk: { translation: uk },
     },
-    lng: 'ru',
     fallbackLng: 'ru',
     supportedLngs: ['ru', 'en', 'uk'],
     interpolation: {
       escapeValue: false,
+    },
+    detection: {
+      order: ['path', 'localStorage', 'navigator'],
+      lookupFromPathIndex: 0,
+      caches: ['localStorage'],
     },
   })
 
